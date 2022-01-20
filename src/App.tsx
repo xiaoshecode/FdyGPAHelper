59e0a0c63e7d3a0a2929627cb7999a4a806f66c5
Wangshuyi <powerfool@Powerfools-Mac.local>
2022-01-20 11:50:35 +0800

    stage: upload multiple .xls files with formData

--- src/App.tsx
+++ src/App.tsx
@@ -1,45 +1,39 @@
-import { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import { Button, Upload } from 'antd'
+import 'antd/dist/antd.css'
+import './App.scss'
+
+const uploadMultipleFiles = () => {
+  const inputEl = document.createElement('input')
+  inputEl.type = 'file'
+  inputEl.multiple = true
+  inputEl.accept = '.xls'
+  inputEl.onchange = (e: any) => {
+    console.log(e);
+    const form = new FormData()
+    const files = Array.from(e.path[0].files)
+    console.log('files', files);
+    files.forEach((f: any) => {
+      form.append(f.name, f)
+    })
+    fetch('/api/upload', {
+      method: 'POST',
+      body: form,
+      mode: 'no-cors'
+    }).then((res) => {
+      console.log(res);
+    })
+    inputEl.remove()
+  }
+  inputEl.click()
+}
 
 function App() {
-  const [count, setCount] = useState(0)
-
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello Vite + React!</p>
-        <p>
-          <button type="button" onClick={() => setCount((count) => count + 1)}>
-            count is: {count}
-          </button>
-        </p>
-        <p>
-          Edit <code>App.tsx</code> and save to test HMR updates.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          {' | '}
-          <a
-            className="App-link"
-            href="https://vitejs.dev/guide/features.html"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Vite Docs
-          </a>
-        </p>
-      </header>
+  return <div>
+    Hello GPA Helper
+    <div>
+      <Button onClick={() => { uploadMultipleFiles() }}>自定义上传</Button>
     </div>
-  )
+  </div>
 }
 
 export default App
--- vite.config.ts
+++ vite.config.ts
@@ -3,5 +3,17 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  plugins: [react()],
+  server: {
+    proxy: {
+      '/api': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      },
+      '/download': {
+        target: 'http://127.0.0.1:5000',
+        changeOrigin: true,
+      }
+    }
+  }
 })
