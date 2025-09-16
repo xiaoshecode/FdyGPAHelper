c500a7c2df59603ee5bc70ffba4992e97b8cd13e
xiaoshe <shejp20@gmail.com>
2025-09-16 18:27:40 +0800

    update version0.2.0

--- src/App.tsx
+++ src/App.tsx
@@ -29,7 +29,7 @@ function App() {
           allRecords.push(...records)
         }
         const rankRecords = calculateGpaAndRank(allRecords)
-        generateExcel(rankRecords, "GPA及排名_"+getCurrentDateTime()+".xlsx")
+        generateExcel(rankRecords, "GPA及排名_" + getCurrentDateTime() + ".xlsx")
         inputEl.remove()
       } finally {
         setLoading(false)
@@ -39,7 +39,7 @@ function App() {
   }
   return <div className='container'>
     <div className='title'>
-      <img src={logo} /> THU GPA Helper @ {process.env.PACKAGE_VERSION}
+      <img src={logo} /> GPAHelper for Fdy @ {process.env.PACKAGE_VERSION}
     </div>
     <Spin spinning={loading}>
       <div className='upload-block' onClick={() => { uploadMultipleFiles() }}>
@@ -50,9 +50,10 @@ function App() {
     </Spin>
     <div className='demonstration'>
       <Title level={3}>使用帮助</Title>
-      <Paragraph>用前须知: <Text mark>本工具仅提供给清华大学辅导员计算所带同学成绩使用，工具不会保存辅导员上传的同学成绩，请放心使用！</Text></Paragraph>
-      <Paragraph>
-        功能介绍:
+      <Paragraph>用前须知: <Text mark>本工具提供给清华大学辅导员计算同学成绩使用，基于Javascript网站计算，不会保存辅导员上传的同学成绩，请放心使用！</Text></Paragraph>另外可以前往<a href='https://github.com/xiaoshecode/FdyGPAHelper' target='_blank'>源代码地址</a>根据README文档自行部署到本地或服务器上使用。
+      
+      <Paragraph>  
+        <Title level={4}>功能介绍</Title>
         <ul>
           <li>
             辅导员上传从信息门户导出的同学成绩（“所有成绩查询”或“近期成绩查询”），工具会根据上传数据计算同学们的<Text strong>必修限选GPA和全部课程GPA</Text>（保留6位小数），
@@ -65,23 +66,38 @@ function App() {
           <li>
             辅导员导出的成绩册文件至少需要包含【姓名】【学号】【教学班级】【成绩】【绩点成绩】【学分】【课程属性】七列。
           </li>
+          <li>
+            源代码地址：<a href='https://github.com/xiaoshecode/FdyGPAHelper' target='_blank'>https://github.com/xiaoshecode/FdyGPAHelper</a>
+          </li>
         </ul>
       </Paragraph>
+      
       <Paragraph>
-        联系方式:（若有 Bug 或建议可联系我）
+        <Title level={4}>TODO</Title>
         <ul>
-          <li>网易邮箱: <Text keyboard>whiteffire@163.com</Text>（常用）</li>
+          <li>GPA成绩完全相同时，排名标准待定</li>
+          <li>本地保存用户名和密码，登录info后直接获取数据生成表格</li>
         </ul>
       </Paragraph>
+      
       <Paragraph>
-        更新日志:
+        <Title level={4}>更新日志</Title>
         <ul>
-          <li>2024.09.08: 去除后端 Python 部分，全部计算均在浏览器上完成；矫正“已修”学分的计算；因原域名(https://gpa-helper.whiteffire.cn)或者证书可能过期，为便于维护，该网站后续将迁移到 Github Pages 上，<a href='https://powerfooi.github.io/gpa-helper' target='_blank'>新网址链接</a></li>
+          <li>2025.09.16: <a href='https://github.com/xiaoshecode' target='_blank'>xiaoshe</a> copy from <a href='https://github.com/PowerfooI' target='_blank'>powerfooi</a>，继续维护</li>
+          <li>2024.09.08: 去除后端 Python 部分，全部计算均在浏览器上完成；
+            矫正“已修”学分的计算；因原域名或者证书可能过期，为便于维护，该网站后续将迁移到 Github Pages 上</li>
           <li>2023.09.24: 修复非整数学分课程在计算时被忽略的问题，例如体疗目前为 0.8 学分</li>
           <li>2023.03.11: GPA相同时设置为相同名次，新增两列“全部课程学分”和“必修限选学分”，<b>仅包含参与计算GPA的学分，如记P/F的课程不在其中</b></li>
           <li>2022.09.30: GPA计算结果保留小数位从3位改为6位</li>
           <li>2022.01.20: 美化用户界面，支持多文件上传</li>
           <li>2021.01.23: 部署GPA计算工具在线版，支持单个文件上传</li>
+        </ul>
+      </Paragraph>
+      <Paragraph>
+        <Title level={4}>最终</Title>
+        <ul>
+          <li>联系方式: <Text keyboard>shejp20@gmail.com</Text></li>
+          <li>感谢原作者 <a href='https://github.com/PowerfooI' target='_blank'>powerfooi</a> 的贡献与支持</li>
         </ul>
       </Paragraph>
     </div>
